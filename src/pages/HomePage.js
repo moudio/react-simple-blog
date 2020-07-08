f62e80a21d91a57ffd629f4093cbff4a6ca3824c
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-07-08 09:51:01 +0000

    Install react-router-dom

---
+++ src/pages/HomePage.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to my blog!</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim aperiam,
+        accusantium modi dolorum minus dolorem asperiores nemo placeat molestias
+        beatae nesciunt dolor veniam voluptatem illo hic odio at eaque? Qui
+        animi voluptate deleniti praesentium quasi laboriosam, aliquam sunt
+        incidunt? Esse explicabo saepe quaerat atque voluptas veniam dolor
+        placeat velit laudantium.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe
+        repellendus, totam commodi corrupti odio debitis sit facilis fuga in ea
+        est officiis dolores dolorum pariatur accusantium sint reprehenderit
+        ducimus repellat praesentium suscipit veritatis vitae modi odit ipsa.
+        Quod, corporis. Sapiente iste, veniam vitae perferendis aperiam quisquam
+        sint, expedita omnis ab voluptas exercitationem quidem facilis
+        necessitatibus nulla voluptatibus eaque architecto ipsa nemo dolores
+        dolor. Reiciendis explicabo cum cumque, blanditiis officia tempora.
+      </p>
+    </div>
+  );
+}
+
+export default HomePage;
