eddacddc2050a81b7f98fcdf61899740f5528e0e
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-07-08 10:38:59 +0000

    Show the first characters of an article on articles listing

--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -9,6 +9,7 @@ function ArticlesList() {
       {articleContent.map((article, key) => (
         <Link to={`/article/${article.name}`} key={key}>
           <h3>{article.title}</h3>
+          <p>{article.content[0].substring(0, 150)}...</p>
         </Link>
       ))}
     </>
