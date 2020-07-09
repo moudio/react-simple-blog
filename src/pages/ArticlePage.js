30cf5796468859caead341ce71f5afbb8cff3a3a
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-07-09 15:39:32 +0000

    Add a comment section to the ArticlePage

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import articleContent from './article-content';
+import CommentsList from '../components/CommentsList';
 import ArticlesList from '../components/ArticlesList';
 import NotFoundPage from './NotFoundPage';
 function ArticlePage({ match }) {
   const name = match.params.name;
   const article = articleContent.find((article) => article.name === name);
+
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(`/api/articles/${name}`);
+      const body = await result.json();
+      setArticleInfo(body);
+    };
+    fetchData();
+  }, [name]);
+
   if (!article) return <NotFoundPage />;
   const otherArticles = articleContent.filter(
     (article) => article.name !== name
@@ -12,10 +25,13 @@ function ArticlePage({ match }) {
   return (
     <>
       <h1>{article.title}</h1>
+      <p>This post has been upvoted {articleInfo.upvotes} times</p>
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
+      <CommentsList comments={articleInfo.comments} />
       <h3>Other Articles</h3>
+
       <ArticlesList articles={otherArticles} />
     </>
   );
